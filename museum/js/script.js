3940e28410305363765a1d17e477d8eff83a03b7
Dzmitry Iliukovich <mr.ilyukovich@gmail.com>
2021-07-27 00:28:28 +0300

    feat: add video & create videoSpriteIcons.svg

---
+++ museum/js/script.js
@@ -0,0 +1,12 @@
+const long = document.getElementById('longRange'),
+  short = document.getElementById('shortRange');
+
+long.addEventListener('input', function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`;
+});
+
+short.addEventListener('input', function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`;
+});
