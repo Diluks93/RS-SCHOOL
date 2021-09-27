e3a1962fb6118162682febe0daf216fefb040114
Dzmitry Iliukovich <mr.ilyukovich@mail.ru>
2021-09-27 23:49:13 +0300

    refactor: deploy

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # diluks93-JSFE2021Q3
 Private repository for @diluks93
+
+https://rolling-scopes-school.github.io/diluks93-JSFE2021Q3/museum
---
+++ museum/js/slider.js
@@ -0,0 +1,7 @@
+export default function slider(){
+const carousel = document.querySelector('.slider'),
+  current = carousel.querySelector('.current'),
+  cubs = carousel.querySelector('.welcome__block-cubs'),
+  arrows = carousel.querySelector('.welcome__block-arrows');
+
+}
