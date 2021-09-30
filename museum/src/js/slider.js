8c7a675728d13edfcc0907827ee7c6f66aad99c7
Dzmitry Iliukovich <mr.ilyukovich@gmail.com>
2021-09-30 12:15:10 +0300

    feat: add picture *.webp

--- museum/src/js/slider.js
+++
@@ -1,7 +0,0 @@
-export default function slider(){
-const carousel = document.querySelector('.slider'),
-  current = carousel.querySelector('.current'),
-  cubs = carousel.querySelector('.welcome__block-cubs'),
-  arrows = carousel.querySelector('.welcome__block-arrows');
-
-}
