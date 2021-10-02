fc44a3774255b4f9481d61bd82215e42a57beb88
Dzmitry Iliukovich <mr.ilyukovich@gmail.com>
2021-10-02 14:40:54 +0300

    feat: add adaptive pallax and gallery

--- museum-adaptive/src/js/hamburger.js
+++ museum-adaptive/src/js/hamburger.js
@@ -1,11 +1,20 @@
 export default function showSidepanel() {
   const hamburger = document.querySelector('.hamburger'),
     sidepanel = document.querySelector('.sidepanel'),
-    welcomeBlock = document.querySelector('.welcome__wrapper');
+    welcomeBlock = document.querySelector('.welcome__wrapper'),
+    welcomeSection = document.querySelector('.welcome__container');
 
   hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger_active');
     sidepanel.classList.toggle('show');
     welcomeBlock.classList.toggle('hide');
   });
+
+  window.onclick = function (event) {
+    if (event.target == welcomeSection) {
+      sidepanel.classList.remove('show');
+      hamburger.classList.remove('hamburger_active');
+      welcomeBlock.classList.remove('hide');
+    }
+  };
 };
