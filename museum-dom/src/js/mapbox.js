6633a8cdb28cccaa46f2ca0be842bdae1adb8615
Dzmitry Iliukovich <mr.ilyukovich@gmail.com>
2021-10-08 21:24:01 +0300

    fix: add icons according to the layout

--- museum-dom/src/js/mapbox.js
+++ museum-dom/src/js/mapbox.js
@@ -3,7 +3,7 @@ export default function useMapbox(){
     'pk.eyJ1IjoiZGlsdWtzIiwiYSI6ImNrdTFxc3ZmODBuazAyb28xcTdtYTZmMGUifQ.oH3L-MEDH588UW1NMgorKg';
   let map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/diluks/ckui98gc8ahw517mp4l5wbwr1',
+    style: 'mapbox://styles/diluks/ckuiiuxe1d31p19nsc7tc6xsp',
     center: [2.336263, 48.860929],
     zoom: 15.71,
   });
@@ -14,5 +14,20 @@ export default function useMapbox(){
   })
 
   map.addControl(nav, 'top-right');
-}
+  let marker1 = new mapboxgl.Marker({ color: 'black' })
+    .setLngLat([2.3364, 48.86091])
+    .addTo(map);
 
+  let marker2 = new mapboxgl.Marker({ color: 'grey' })
+    .setLngLat([2.3333, 48.8602])
+    .addTo(map);
+  let marker3 = new mapboxgl.Marker({ color: 'grey' })
+    .setLngLat([2.3397, 48.8607])
+    .addTo(map);
+  let marker4 = new mapboxgl.Marker({ color: 'grey' })
+    .setLngLat([2.333, 48.8619])
+    .addTo(map);
+  let marker5 = new mapboxgl.Marker({ color: 'grey' })
+    .setLngLat([2.3365, 48.8625])
+    .addTo(map);
+}
