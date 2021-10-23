a3a54adf975b0a91580de12ac0752b2a37c6a341
Dzmitry Iliukovich <mr.ilyukovich@gmail.com>
2021-10-23 16:36:01 +0300

    refactor: pro player

--- momentum/src/js/player.js
+++ momentum/src/js/player.js
@@ -1,8 +1,6 @@
 import { greetingTranslation } from './getGreeting.js';
 import playList from './playList.js';
 
-//todo volume progress не работает должным образом подключить прогресс бар и громкость из музея и костомизовать оттуда же
-
 const BTNS = document.querySelectorAll('.play'),
   BTN_PREV = document.querySelector('.play-prev'),
   BTN_NEXT = document.querySelector('.play-next'),
@@ -11,7 +9,6 @@ const BTNS = document.querySelectorAll('.play'),
   DURATION = document.querySelector('.duration'),
   AUDIO_PLAYER_BTN = document.querySelector('.player-title'),
   AUDIO_PLAYER = document.querySelector('.audioplayer'),
-  TIME_LINE = AUDIO_PLAYER.querySelector('.timeline'),
   PROGRESS_BAR = AUDIO_PLAYER.querySelector('.progress-bar'),
   PLAY_NAME = AUDIO_PLAYER.querySelector('.play-name'),
   VOLUME_SLIDER = AUDIO_PLAYER.querySelector('.volume-slider'),
@@ -29,6 +26,7 @@ function playAudio() {
   DURATION.textContent = `${playList[playNum].duration}`;
   PLAY_NAME.textContent = playList[playNum].title;
   audio.currentTime = 0; 
+  console.log(audio.currentTime)
   if(isPlay) audio.play();
 }
 
@@ -72,21 +70,21 @@ function playNext(){
 playList.forEach(el => {
   li = document.createElement('li');
   li.classList.add('play-item');
-  li.textContent = `${el.title}`;
+  li.innerHTML = `<span>${el.title}</span><span>${el.duration}</span>`
   PLAYLIST_CONTAINER.append(li);
 });
 
 BTNS.forEach(BTN => BTN.addEventListener('click', () => {
   toggleBtn();
   setInterval(function() {
-        let audioTime = Math.round(audio.currentTime);
-        let audioLength = Math.round(audio.duration);
-        PROGRESS.style.width = (audioTime * 100) / audioLength + '%';
-    }, 1000)
+    let audioTime = Math.round(audio.currentTime);
+    let audioLength = Math.round(audio.duration);
+    PROGRESS.style.width = (audioTime * 100) / audioLength + '%';
+  }, 1000)
 }));
 
 function audioChangeVolume() {
-  let volume = volumeScale.value / 100;
+  let volume = VOLUME_SCALE.value / 100;
   audio.volume = volume;
   if (audio.volume == 0) {
     MUTE_BTN.classList.remove('audio-mute-false');
@@ -97,11 +95,20 @@ function audioChangeVolume() {
   }
 }
 
-VOLUME_SCALE.addEventListener('change', audioChangeVolume);
+VOLUME_SCALE.addEventListener('change', (e)=>{
+  audioChangeVolume();
+  let value = e.target.value;
+  VOLUME_SCALE.style.background = getPattern(value);
+});
 
-MUTE_BTN.addEventListener('click', ()=> {
+MUTE_BTN.addEventListener('click', (e) => {
+  let value = e.offsetX;
   audio.muted = !audio.muted;
-  MUTE_BTN.classList.toggle('audio-mute')
+  MUTE_BTN.classList.toggle('audio-mute');
+  if (audio.muted)
+    VOLUME_SCALE.style.background = 'var(--background-color) 100%';
+  else 
+    VOLUME_SCALE.style.background = getPattern(value);
 })
 BTN_PREV.addEventListener('click', playPrev);
 BTN_NEXT.addEventListener('click', playNext);
@@ -120,32 +127,50 @@ audio.addEventListener(
   false
 );
 
-TIME_LINE.addEventListener(
-  'click',
-  (e) => {
-    const timelineWidth = getComputedStyle(TIME_LINE).width;
-    const timeToSeek = (e.offsetX / parseInt(timelineWidth)) * audio.duration;
-    audio.currentTime = timeToSeek;
-  },
-  false
-);
+function getPattern(value) {
+  return `linear-gradient(
+    to right,
+    var(--main-color) 0%,
+    var(--main-color) ${value}%,
+    var(--background-color) ${value}%,
+    var(--background-color) 100%
+  )`;
+}
 
 VOLUME_SLIDER.addEventListener(
   'click',
   (e) => {
     const sliderWidth = getComputedStyle(VOLUME_SLIDER).width;
-    const newVolume = e.offsetX / parseInt(sliderWidth);
+    let newVolume = e.offsetX / parseInt(sliderWidth);
+    if (newVolume < 0) newVolume = 0;
+    if (newVolume > 100) newVolume = 1;
     audio.volume = newVolume;
-    VOLUME_SCALE.style.width = newVolume * 100 + '%';
   },
   false
 );
 
-setInterval(() => {
-  PROGRESS_BAR.style.width = (audio.currentTime / audio.duration) * 100 + '%';
+setTimeout(() => {
+  PROGRESS_BAR.value = '0';
+  PROGRESS_BAR.style.background = 'var(--background-color)';
+}, 0);
+
+audio.addEventListener('timeupdate', updateProgressBar);
+PROGRESS_BAR.addEventListener('input', liveSearchProgress);
+
+function liveSearchProgress() {
+  const value = this.value;
+  audio.currentTime = (value * audio.duration) / 100;
+  this.style.background = getPattern(value);
+}
+
+function updateProgressBar() {
+  const position = (audio.currentTime / audio.duration) * 100;
+  PROGRESS_BAR.value = position;
+  PROGRESS_BAR.style.background = getPattern(position);
+  getTimeCodeFromNum();
   AUDIO_PLAYER.querySelector('.duration-timer .current').textContent =
     getTimeCodeFromNum(audio.currentTime);
-}, 500);
+}
 
 function getTimeCodeFromNum(num) {
   let seconds = parseInt(num);
