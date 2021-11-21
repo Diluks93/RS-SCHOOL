358323a2aebc57696593fcb7097ebec2ea5a1217
Dzmitry Iliukovich <mr.ilyukovich@gmail.com>
2021-11-21 11:47:15 +0300

    fix: page categories, add class active, hover effect

--- art-quiz/src/module/createCards.js
+++ art-quiz/src/module/createCards.js
@@ -3,28 +3,31 @@ import { getLinkToJSON } from './getImages';
 export const CARDS = document.querySelector('.cards');
 
 export async function createCards() {
+  const data = await getLinkToJSON();
   let previewElement;
   isPicture ? (previewElement = -1) : (previewElement = 12);
 
   for (let i = 1; i <= 12; i++) {
-  let li = document.createElement('li');
+    let li = document.createElement('li');
     li.classList.add('cart', 'category__cart');
-    const data = await getLinkToJSON();
+
     li.innerHTML = `
-      <h4 class="title__cart">Round ${i} <span class="result">0/10</span></h4>
-      <figure>
-        <picture>
-            <source srcset="https://raw.githubusercontent.com/Diluks93/image-data/master/img/${
-              data[previewElement + i][0].imageNum + '.webp'
-            }">
-            <img class="img img-category" src="https://raw.githubusercontent.com/Diluks93/image-data/master/img/${
-              data[previewElement + i][0].imageNum + '.jpg'
-            }" alt="preview category">
-        </picture>
-        <figcaption class="category__descr">Play again</figcaption>
-      </figure>
+      <a href="#round${i}">
+        <h4 class="title__cart">Round ${i} <span class="result">0/10</span></h4>
+        <figure>
+          <picture>
+              <source srcset="https://raw.githubusercontent.com/Diluks93/image-data/master/img/${
+                data[previewElement + i][0].imageNum + '.webp'
+              }">
+              <img class="img img-category" src="https://raw.githubusercontent.com/Diluks93/image-data/master/img/${
+                data[previewElement + i][0].imageNum + '.jpg'
+              }" alt="preview category">
+          </picture>
+          <figcaption class="category__descr">Play again</figcaption>
+        </figure>
+      </a>
     `;
 
     CARDS.append(li);
-  }
-}
+  };
+};
