b2c7b9bd116c8535c61dab62ccfa13c2a47ed914
Dzmitry Iliukovich <mr.ilyukovich@gmail.com>
2021-11-28 10:28:35 +0300

    feat: add apiKey for the application to work

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '9725a13c526f4f038a1dc65703a65bac',
         });
     }
 }
