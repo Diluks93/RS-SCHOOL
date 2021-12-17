ed38b56a7358f057170acfd67b677d1ea43558b5
Dzmitry Iliukovich <mr.ilyukovich@gmail.com>
2021-12-17 21:48:59 +0300

    refactor: add new function and create variable according to the mentor's requirements

--- migration-newip-to-ts/src/components/view/news/news.ts
+++ migration-newip-to-ts/src/components/view/news/news.ts
@@ -11,21 +11,16 @@ class News implements ClassNews {
     const newsItemTemp: HTMLTemplateElement = document.querySelector('#newsItemTemp') as HTMLTemplateElement;
 
     news.forEach((item: ObjectArticles, idx: number): void => {
-      const newsClone: HTMLTemplateElement = newsItemTemp.content.cloneNode(true) as HTMLTemplateElement;
+      const newsClone: HTMLTemplateElement = newsItemTemp.content.cloneNode(true) as HTMLTemplateElement,
+        URL: string = item.urlToImage || 'img/news_placeholder.jpg';
 
       if (idx % 2) {
         (newsClone.querySelector(EnumNews.item) as HTMLTemplateElement).classList.add('alt');
       }
 
-      (newsClone.querySelector(EnumNews.photo) as HTMLTemplateElement).style.backgroundImage = `url(${
-        item.urlToImage || 'img/news_placeholder.jpg'
-      })`;
+      (newsClone.querySelector(EnumNews.photo) as HTMLTemplateElement).style.backgroundImage = `url(${URL})`;
       (newsClone.querySelector(EnumNews.author) as HTMLTemplateElement).textContent = item.author || item.source.name;
-      (newsClone.querySelector(EnumNews.date) as HTMLTemplateElement).textContent = item.publishedAt
-        .slice(0, 10)
-        .split('-')
-        .reverse()
-        .join('-');
+      (newsClone.querySelector(EnumNews.date) as HTMLTemplateElement).textContent = this.getDate(item);
 
       (newsClone.querySelector(EnumNews.title) as HTMLTemplateElement).textContent = item.title;
       (newsClone.querySelector(EnumNews.source) as HTMLTemplateElement).textContent = item.source.name;
@@ -37,6 +32,10 @@ class News implements ClassNews {
 
     (document.querySelector(EnumNews.news) as HTMLTemplateElement).innerHTML = '';
     (document.querySelector(EnumNews.news) as HTMLTemplateElement).appendChild(fragment);
+  }
+
+  private getDate(item: ObjectArticles): string {
+    return item.publishedAt.slice(0, 10).split('-').reverse().join('-');
   }
 }
 
